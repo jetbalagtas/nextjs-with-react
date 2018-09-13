d81b08d91dbf3b213b4a9038145706cf40c56d91
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-12 21:59:52 -0400

    using components with nextjs pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
   <div>
     <h1>The Auth Index Page</h1>
+    <User name='Jet' age={28} />
   </div>
 );
 
